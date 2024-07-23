f5fe74bfbec6dd8aa6dc5813fcbb70a7d7141397
GGK <kghulamghouse@gmail.com>
2024-07-23 13:25:40 +0530

    Episode-02(Igniting App & Parcel)

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { }, "I am Good!"),
-    React.createElement("h2", { }, "I am Good!"),
-    React.createElement("h3", { }, "I am Good!"),
+    React.createElement("h1", { key: "h1" }, "I am Good!"),
+    React.createElement("h2", { key: "h2" }, "I am Good!"),
+    React.createElement("h3", { key: "h3" }, "I am Good!"),
   ])
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
