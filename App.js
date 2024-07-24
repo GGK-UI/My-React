a56dc738bbe27bc5e70fc9ab38861f3fef1026bc
GGK <kghulamghouse@gmail.com>
2024-07-24 13:19:18 +0530

    Episode-03(Laying The Foundation)

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { key: "h1" }, "I am Good!"),
-    React.createElement("h2", { key: "h2" }, "I am Good!"),
-    React.createElement("h3", { key: "h3" }, "I am Good!"),
-  ])
+// React Element
+const heading = (
+  <h1 id="heading" tabIndex="5">
+    I am Just React Element
+  </h1>
 );
+
+// React Component
+
+const TitleComponent = () => (
+  <h1 id="heading" tabIndex="5">
+    I am Just React Title Component
+  </h1>
+);
+const num = 100;
+
+const HeadingComponent = () => (
+  <div id="container">
+    {heading}
+    {TitleComponent()}
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    {num + 10}
+    <h1 className="heading">I am Heading Function Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
